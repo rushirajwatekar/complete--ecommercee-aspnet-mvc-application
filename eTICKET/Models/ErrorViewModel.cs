ce4e9452fde896e8c222d668380dc906d5068a80
Rushiraj Watekar <2121330@cognizant.com>
2023-06-20 15:16:51 +0530

    Add project files.

---
+++ eTICKET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eTICKET.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
